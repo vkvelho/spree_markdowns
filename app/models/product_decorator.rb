a2eec2ddb243159f376042b80e6291302a2c4d75
Ilkka Sopanen <ilkka.sopanen@gmail.com>
2013-07-26 02:04:46 +0300

    Product decorator added

---
+++ app/models/product_decorator.rb
@@ -0,0 +1,9 @@
+require 'redcarpet'
+
+Product.class_eval do
+  def description_html
+    desc = description.empty? ? 'This product has no description' : description
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(desc)
+  end
+end
