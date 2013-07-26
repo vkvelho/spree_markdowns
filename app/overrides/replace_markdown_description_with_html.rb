e327c2fbfd86c3497a778c36136c07478ae56236
Ilkka Sopanen <ilkka.sopanen@gmail.com>
2013-07-26 12:48:45 +0300

    Replaced the dummy description with real one

--- app/overrides/replace_markdown_description_with_html.rb
+++ app/overrides/replace_markdown_description_with_html.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'spree/products/show',
   :name => 'replace_markdown_description_with_html',
   :replace => 'div[data-hook="description"]',
-  :text => '<h2>Testing replacement of description</h2>')
+  :text => '<%= raw @product.description_html %>')
