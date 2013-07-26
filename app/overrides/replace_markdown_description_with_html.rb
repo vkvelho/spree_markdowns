92c3d5e9708c38bbfc03623229152b31cd291460
Ilkka Sopanen <ilkka.sopanen@gmail.com>
2013-07-26 03:33:04 +0300

    Changed the virtual path again

--- app/overrides/replace_markdown_description_with_html.rb
+++ app/overrides/replace_markdown_description_with_html.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => 'spree/frontend/products/show',
+Deface::Override.new(:virtual_path => 'spree/products/show',
   :name => 'replace_markdown_description_with_html',
   :replace => 'div[data-hook="description"]',
   :text => '<h2>Testing replacement of description</h2>')
