f326fab334e3bba637b6b3c4bea860ea289673af
Ilkka Sopanen <ilkka.sopanen@gmail.com>
2013-07-26 03:18:11 +0300

    Testing replacement with deface

---
+++ app/overrides/replace_markdown_description_with_html.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => 'spree/frontend/app/views/spree/products/show',
+  :name => 'replace_markdown_description_with_html',
+  :replace => 'div[data-hook="description"]',
+  :text => '<h2>Testing replacement of description</h2>')
