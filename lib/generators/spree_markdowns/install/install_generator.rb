ea0978a890f057a6fa870555c8e4fa783e66b9a2
Ilkka Sopanen <ilkka.sopanen@gmail.com>
2013-10-30 17:58:29 +0200

    Confused SourceTree commits...

--- lib/generators/spree_markdowns/install/install_generator.rb
+++ lib/generators/spree_markdowns/install/install_generator.rb
@@ -5,12 +5,12 @@ module SpreeMarkdowns
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_markdowns\n"
+        # append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_markdowns\n"
         append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_markdowns\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_markdowns\n", :before => /\*\//, :verbose => true
+        # inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_markdowns\n", :before => /\*\//, :verbose => true
         inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_markdowns\n", :before => /\*\//, :verbose => true
       end
 
