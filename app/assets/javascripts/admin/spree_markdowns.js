e045be596e0459b2ea26141019cb77381de1c87d
Ilkka Sopanen <ilkka.sopanen@gmail.com>
2013-11-12 17:25:50 +0200

    Changed the button to discrete anchor

--- app/assets/javascripts/admin/spree_markdowns.js
+++ app/assets/javascripts/admin/spree_markdowns.js
@@ -3,20 +3,14 @@
 
 $(function() {
 
-  $('<a href="#" class="button icon-eye-close" title="Click with confidence. Live preview makes no changes." icon="icon-eye-close" id="product_description_preview_button">Live preview</a>').appendTo('#product_description_field');
-
+  $('<a href="#" class="icon-eye-open" title="Click with confidence. Live preview makes no changes." icon="icon-eye-close" id="product_description_preview_button">Toggle HTML-preview</a>').appendTo('#product_description_field');
+  hideDescriptionPreview();
+  
   $('#product_description_preview_button').click(function(e) {
-
     if($('#product_description_preview').is(":visible")) {
-      $('#product_description_preview').slideUp();
-      $('#product_description_preview_button')
-        .addClass("icon-eye-close")
-        .removeClass("icon-eye-open");
+      hideDescriptionPreview();  
     } else {
-      $('#product_description_preview').slideDown();
-      $('#product_description_preview_button')
-        .removeClass("icon-eye-close")
-        .addClass("icon-eye-open");
+      showDescriptionPreview();
     }
 
     e.preventDefault();
@@ -26,4 +20,18 @@ $(function() {
   $('#product_description').bind('keyup', function() {
     $('#product_description_preview').html(markdown.toHTML($('#product_description').val()));
   });
+
+  function hideDescriptionPreview() {
+    $('#product_description_preview').slideUp();
+    $('#product_description_preview_button')
+      .addClass("icon-eye-close")
+      .removeClass("icon-eye-open");
+  } 
+
+  function showDescriptionPreview() {
+    $('#product_description_preview').slideDown();
+    $('#product_description_preview_button')
+      .removeClass("icon-eye-close")
+      .addClass("icon-eye-open");
+  }
 });
